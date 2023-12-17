bca50dec4aa64d45b1709c63312abfb24c55b841
mairaabbas <139677582+mairaabbas@users.noreply.github.com>
2023-12-17 13:48:31 +0500

    Initial commit

---
+++ basics.py
@@ -0,0 +1,63 @@
+print("hello world")
+print("hiiiiiiiii")
+
+print("welcome to the world")
+
+#variables
+#strings
+#numbers
+#floating numbers
+#boolean
+
+name = "maira"
+
+age = 21
+
+radius = 22.0
+
+print(name)
+print(age)
+print(radius)
+
+#exercise #1
+
+personame = ("tony shark")
+
+age = 51
+
+tony = ("is genius")
+
+print(personame)
+print(age)
+print(tony)
+
+first_name =("Tony")
+last_name =("shark")
+age = 51
+is_genius = True
+
+print(first_name+last_name)
+print(age)
+print(is_genius)
+
+#exercise #2
+
+superhero = input("what is your superhero name?")
+print(superhero)
+
+first =input("enter your first number : ")
+second =input ("enter your second number")
+
+sum =int(first) + int(second)
+
+print("the sum value is " + str(sum))
+
+name = "maira abbas"
+
+print(name.upper ())
+
+print(name.lower())
+
+print(name.find("a"))
+print(name.replace("maira abbas", "talented"))
+print("t" in name)
